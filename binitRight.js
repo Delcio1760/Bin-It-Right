8997f546c0ff2e73de4cbe5b34c54c23e407e65d
delcio cunha <delcioaugustocunha@gmail.com>
2025-10-30 15:23:33 +0000

    Atualizações no Js e no Css

--- binitRight.js
+++ binitRight.js
@@ -1,11 +1,11 @@
 const canvas = document.getElementById('gameCanvas');
 const ctx = canvas.getContext('2d');
 
-canvas.width = window.innerWidth;
-canvas.height = window.innerHeight;
+canvas.width = 1100;
+canvas.height = 800;
 
 const backgroundImage = new Image();
-backgroundImage.src = './images/gameBg.jpeg';
+backgroundImage.src = './Images/gameBg.jpeg';
 
 backgroundImage.onload = () => {
     ctx.drawImage(backgroundImage, 0, 0, canvas.width, canvas.height);

79c61a4953f2e77aac1e909104801235e0c50dde
delcio cunha <delcioaugustocunha@gmail.com>
2025-11-03 16:01:16 +0000

    Major updates and animation

--- binitRight.js
+++ binitRight.js
@@ -24,14 +24,41 @@ yellowbinImage.src = './Images/yellowbin.png'; // Caixote amarelo
 const blackbinImage = new Image();
 blackbinImage.src = './Images/blackbin.png'; // Caixote preto
 
-setTimeout(() => {
-    // Desenhar fundo
-    ctx.drawImage(backgroundImage, 0, 0, canvas.width, canvas.height);
+// Imagem do rato (sprite)
+const ratoImg = new Image();
+ratoImg.src = './Images/mouse.png'; // Imagem do rato
+
+let ratoX = -150;
+let ratoY = 680;
+let frameIndex = 0;
+
+function Desenhar(){
+    // desenhar fundo
+    ctx.drawImage(backgroundImage, 0,0,canvas.width,canvas.height);
+
+    // desenhar caixotes de lixo
+    ctx.drawImage(redbinImage, 76, 600, 100, 100);
+    ctx.drawImage(bluebinImage, 350, 600, 100, 100);
+    ctx.drawImage(greenbinImage, 600, 600, 100, 100);
+    ctx.drawImage(yellowbinImage, 800, 600, 100, 100);
+    ctx.drawImage(blackbinImage, 1000, 600, 100, 100);
     
-    // Desenhar os 5 caixotes todos na mesma linha
-    ctx.drawImage(redbinImage, 76, 600, 100, 100);      // Vermelho
-    ctx.drawImage(bluebinImage, 350, 600, 100, 100);     // Azul
-    ctx.drawImage(greenbinImage, 600, 600, 100, 100);    // Verde
-    ctx.drawImage(yellowbinImage, 800, 600, 100, 100);  // Amarelo
-    ctx.drawImage(blackbinImage, 1000, 600, 100, 100);   // Preto
-}, 1000);
+    // desenhar rato
+
+    ctx.drawImage(ratoImg, frameIndex * 161.5,0,161.5,47, ratoX, ratoY,161.5,47);
+    ratoX = ratoX +50; // velocidade do rato
+
+    if(ratoX > 1100){
+        ratoX = -150; // Se sair do ecrã, volta ao inicio
+    }
+
+    frameIndex++;
+    if(frameIndex == 6){
+        frameIndex = 0; // reiniciar animação do rato
+    }   
+}
+
+// Esperar imagens carregarem antes de desenhar
+ratoImg.onload = function(){setInterval(Desenhar, 1000/14)}; // Desenhar a 12 fps
+
+
